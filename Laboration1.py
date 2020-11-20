36ae708acb34c2f8a79ee19286a411a373802005
ErikSandbecker <63230334+ErikSandbecker@users.noreply.github.com>
2020-11-20 11:39:38 +0100

    abc

--- Laboration1.py
+++ Laboration1.py
@@ -2,29 +2,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random as random
 
-
 class Layer:
-    def __init__(self, nodes, weights, activation_function):
-        self.nodes = nodes
-        self.acivation_function = activation_function
-        for i in range(layer_size):
-            nodes.add(Node(layer_size))
+    def __init__(self, weights, activationFunction):
+        self.nodes = []
+        self.activationFunction = activationFunction
+
+        for i in range(weights):
+            nodes.append(ComplexNode(weights[i]))
+
+class InputLayer:
+    def __init__(self, x):
+        self.nodesList = []
+
+        for i in range(x):
+            nodes = []
+            for j in range(len(x[i] -1)):
+                nodes.append(Node(x[i][j]))
+
+            nodesList.append(nodes)
+
+
 
 class Node:
 
-    def __init__(self, value):
+    def __init__(self, value, layerIndex):
         self.value = value
+        self.layerIndex = layerIndex
 
     def getValue(self):
         return self.value
 
-class ComplexNode(Node):
-    def __init__(self, no_weights):
-        self.b = random(0, 1)
-        self.weights = []
-        for i in range(no_weights - 1):
-            self.weights.add(random(0, 1))
 
+class ComplexNode(Node):
+    def __init__(self, weight, layerIndex):
+        super(ComplexNode, self).__init__(0)
+        self.b = random.random(0, 1)
+        self.weight = weight
+        self.layerIndex = layerIndex
 
     def forward(self):
         pass
@@ -32,29 +46,50 @@ class ComplexNode(Node):
     def backprop(self):
         pass
 
+
 class MLP:
     def __init__(self):
         self.layers = []
+        self.layerIndex = 0
 
-    def addInputLayer(self, inputs):
+    def addInputLayer(self, inputs, activationFunction):
         nodes = []
         for i in range(len(inputs)):
-            nodes.append(Node(inputs[i], None, None))
+            nodes.append(Node(inputs[i], self.layerIndex))
 
-        self.layers.append(Layer(nodes))
+        self.layers.append(Layer(nodes, activationFunction))
+        self.layerIndex+=1
 
-    def add_complexlayer(self, inputs, weights):
+    def addComplexLayer(self, noNodes, activationFunction):
         nodes = []
-        for i in range(len(inputs)):
-            nodes.append(Node(inputs[i], weights, None))
+        for i in range(len(noNodes)):
+            nodes.append(ComplexNode(random.random(0, 1), self.layerIndex))
 
-        self.layers.append(Layer(inputs, weights, None))
+        self.layers.append(Layer(Nodes, activationFunction))
+        self.layerIndex += 1
+
+    def addOutputLayer(self, classes, activationFunction):
+        nodes = []
+        for i in range(len(classes)):
+            nodes.append(ComplexNode(random.random(0, 1), self.layerIndex))
+
+        self.layers.append(Layer(Nodes, activationFunction))
+        self.layerIndex += 1
 
     def _backprop(self, x, y, d_loss, learning_rate):
         pass
 
-    def train(self, x, y, learning_rate=0.01, n_epochs=100):
-        pass
+    def train(self, x, y, learning_rate=0.01, n_epochs=100, noHiddenLayers, noNodes):
+        self.addInputLayer(x, activationFunction)
+        for i in range(noHiddenLayers):
+            self.addComplexLayer(noNodes, sig()) #SIG HÄR
+
+        self.addOutputLayer(1, lin()) #LIN HÄR
+
+        for i in range(n_epochs):
+            for j in range(len(x)):
+                self.layers[0].nodesList[j]
+
 
     def predict(self, x, y):
         sigmoid = lambda z: 1 / (1 + np.e ** (-z))
