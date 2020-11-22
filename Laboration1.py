b5182f381c72924ee9807330625107ab197fdbfd
Emil Malmgren <EmilMa@centiro.se>
2020-11-22 18:11:22 +0100

    söndagsarbete

--- Laboration1.py
+++ Laboration1.py
@@ -14,6 +14,9 @@ class LossFunction:
         return 2 * (predictions - correct_outputs)
 
 class ActivationFunction:
+    def __init__(self):
+        pass
+
     @staticmethod
     def forward(z): pass
 
@@ -25,7 +28,7 @@ class LinearActivationFunction(ActivationFunction):
     def forward(z): return z
 
     @staticmethod
-    def backward(z): return np.ones(z.size)
+    def backward(z): return np.ones(1)
 
 class SigmoidActivationFunction(ActivationFunction):
     @staticmethod
@@ -40,7 +43,7 @@ class Layer:
         self.values = []
 
         for i in range(numberOfNodesPerHiddenLayer):
-            self.nodes.append(ComplexNode(numberOfWeights,activationFunction))
+            self.nodes.append(ComplexNode(numberOfWeights, activationFunction))
 
 
 class InputLayer:
@@ -48,9 +51,9 @@ class InputLayer:
         self.nodes = []
 
     def addData(self, x):
-        for i in range(x):
+        for i in range(len(x)):
             nodes = []
-            for j in range(len(x[i] -1)):
+            for j in range(len(x[i])):
                 nodes.append(Node(x[i][j]))
 
             self.nodes.append(nodes)
@@ -72,32 +75,33 @@ class ComplexNode(Node):
         self.dw = []
         self.db = 0
         super(ComplexNode, self).__init__(0)
-        self.b = random.random(0, 1)
+        self.b = random.random()
         self.weights = []
         for i in range(numberOfWeights):
-            self.weights.append(random.random(0, 1))
+            self.weights.append(random.random())
 
     def forward(self, values):
         z = 0
-        for k in range(values):
-            z += (values[k] * self.weights[k])
-        z += b
+        for k in range(len(values)):
+            z += (float(values[k]) * self.weights[k])
+        z += self.b
         self.value = self.activationFunction.forward(z)
 
-
     def backprop(self, values):
-        for i in range(self.weights):
-            self.dw.append(self.dLoss*self.activationFunction.backward(self.value)*values[i])
-        self.db = self.dLoss*self.activationFunction.backward(self.value)
+        dws = []
+        for i in range(len(self.weights)):
+            dws.append(self.dLoss * self.activationFunction.backward(self.value)*values[i])
+        self.dw = dws
+        self.db = self.dLoss * self.activationFunction.backward(self.value)
 
     def updateDLoss(self, dbs, weights):
         sum = 0
         for i in range(dbs):
-            sum+= dbs[i]*weights[i]
+            sum += dbs[i] * weights[i]
         self.dLoss = sum
 
-    def updateOutputDLoss(self,actualValue):
-        self.dLoss = 2* (actualValue - self.value)
+    def updateOutputDLoss(self, actualValue):
+        self.dLoss = 2 * (float(actualValue) - self.value)
 
     def getDb(self):
         return self.db
@@ -106,21 +110,15 @@ class ComplexNode(Node):
         return self.weights
 
     def updateWeightsAndB(self, lRate):
-        self.weights -= self.dw *lRate
+        self.weights -= float(self.dw) * lRate
         self.b -= self.db * lRate
 
 
 class MLP:
     def __init__(self, data, numberOfHiddenLayers, numberOfNodesPerHiddenLayer):
         self.layers = []
-        self.targets = []
-
-        for i in range(len(data)):
-            self.targets.append(data[i][0].pop())
-
-        self.data = data
         self.addInputLayer()
-        self.addComplexLayer(numberOfNodesPerHiddenLayer, SigmoidActivationFunction, len(data[0]))
+        self.addComplexLayer(numberOfNodesPerHiddenLayer, SigmoidActivationFunction, len(data[0])-1)
         for i in range(numberOfHiddenLayers -1):
             self.addComplexLayer(numberOfNodesPerHiddenLayer, SigmoidActivationFunction, numberOfNodesPerHiddenLayer)
 
@@ -135,47 +133,46 @@ class MLP:
     def addOutputLayer(self, classes, activationFunction, numberOfNodesPerHiddenLayer):
         self.layers.append(Layer(classes, activationFunction, numberOfNodesPerHiddenLayer))
 
+    def train(self, data, learning_rate=0.01, n_epochs=100):
+        targets = []
+        for i in range(len(data)):
+            targets.append(data[i].pop(0))
 
-    def _backprop(self, x, y, d_loss, learning_rate):
-        pass
-
-    def train(self, x, y, learning_rate=0.01, n_epochs=100):
-        self.layers[0].addData(x)
+        self.layers[0].addData(data)
         for i in range(n_epochs):
             #forward
-            for row in self.layers[0].nodes:
-                for j in range(1, layers):
-                    values = []
-                    for previousNode in layers[j - 1].nodes:
+            for row in range(len(self.layers[0].nodes)):
+                values = []
+                for node in self.layers[0].nodes[row]:
+                    values.append(node.value)
+                for node in self.layers[1].nodes:  # för varje nod i första hidden layer
+                    node.forward(values)
+                values = []
+
+                for j in range(2, len(self.layers)):
+                    for previousNode in self.layers[j -1].nodes:
                         values.append(previousNode.getValue())
-                    layers[j].values = values
-                    for node in layers[j].nodes: #för varje nod i första hidden layer
+                    self.layers[j].values = values
+                    values = []
+                    for node in self.layers[j].nodes: #för varje nod i första hidden layer
                         node.forward(values)
 
-                layers[len(layers)-1][0].updateOutputDLoss(self.targets[row])
-                layers[len(layers) - 1][0].backprop(values)
+                #Calculate output loss
+                self.layers[len(self.layers)-1].nodes[0].updateOutputDLoss(targets[row])
+                self.layers[len(self.layers) - 1].nodes[0].backprop(self.layers[len(self.layers) -1].values)
 
-                for layer in range(len(layers)-2,1,-1):
+                #backprop
+                for layer in range(len(self.layers)-2, 1, -1):
                     dbs = []
                     weights = []
-                    for previousNode in layers[layer - 1].nodes:
+                    for previousNode in self.layers[layer - 1].nodes:
                         dbs.append(previousNode.getDb())
                         weights.append(previousNode.getWeights())
                         previousNode.updateWeightsAndB(learning_rate)
 
-                    for node in range(layers[layer].nodes): #för varje nod i första hidden layer
-                        node.updateDLoss(dbs[node],weights[node])
-                        node.backprop(values)
-
-
-            #loss
-            layers[len(layers -1)]
-
-
-            #backward
-            for j in range(len(layers), 1, -1):
-
-
+                    for node in range(self.layers[layer].nodes): #för varje nod i första hidden layer
+                        node.updateDLoss(dbs[node], weights[node])
+                        node.backprop(self.layers[layer + 1])
 
     def predict(self, x, y):
         sigmoid = lambda z: 1 / (1 + np.e ** (-z))
@@ -191,15 +188,15 @@ class MLP:
             #         pass
 
 
-
 dataset = []
-with open("C:\\Users\erik\PycharmProjects\ML2Labb1\\venv\datasets\\boston.csv", 'r') as file:
+with open("C:/Users/EmilMa/Skolarbete/repos/MachineLearning2Lab1/datasets/boston.csv", 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         dataset.append(row)
+mlp = MLP(dataset, 2, 4)
 
 numberOfHiddenLayers = 2
 numberOfNodesPerHiddenLayer = 4
 mlp = MLP(dataset, numberOfHiddenLayers, numberOfNodesPerHiddenLayer)
-
-
+mlp.train(dataset)
+x =5
